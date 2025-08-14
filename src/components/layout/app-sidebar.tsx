7f5375ccdd770661b87a821b68ccd731ea8a22fe
Dương Quang Thoại <quangteotp@gmail.com>
2025-08-14 16:21:23 +0000

    Hãy điều chỉnh lại sidebar hoặc cỡ font của dòng chữ tiêu đề logo trang

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -46,7 +46,7 @@ export default function AppSidebar() {
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b border-sidebar-border px-4 lg:h-[60px] lg:px-6">
           <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
-            <span className="font-headline">CHUẨN TIẾP CẬN PHÁP LUẬT</span>
+            <span className="font-headline text-sm">CHUẨN TIẾP CẬN PHÁP LUẬT</span>
           </Link>
         </div>
         <div className="flex-1">
